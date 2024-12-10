32886649d2f3590dfc963bdeba24b045a42885c2
Jorge Antonio <matroid@outlook.com>
2024-12-10 18:32:25 +0000

    update reuqirements and crew.py

--- src/atoma_agentic_workflows/crew.py
+++ src/atoma_agentic_workflows/crew.py
@@ -1,5 +1,6 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import RagTool
 
 # If you want to run a snippet of code before or after the crew starts, 
 # you can use the @before_kickoff and @after_kickoff decorators
